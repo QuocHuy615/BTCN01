5b30c9b09cf6489513df4124fbabe1a2be1f5935
23120272 <lehuy1519@gmail.com>
2025-10-18 19:52:18 +0700

    feat(footer): tạo hiệu ứng hover, click hover đồng bộ với nav

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 $("nav li").on("click", function() {
+    const index = $(this).index();
+
     $("nav li").removeClass("active");
-    $(this).addClass("active")
+    $(this).addClass("active");
+
+    $("#footer a").removeClass("active").eq(index).addClass("active");
+});
+
+$("#footer a").on("click", function() {
+    const index = $(this).index("#footer a");
+
+    $("#footer a").removeClass("active");
+    $(this).addClass("active");
+
+    $("nav li").removeClass("active").eq(index).addClass("active");
 });
